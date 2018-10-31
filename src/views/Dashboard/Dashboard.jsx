1f640bbd789e16ab3f36afd81b94dd412386150b
EvgeniiPresayzen <midnightzp95@gmail.com>
2018-10-31 17:36:15 +0200

    fixed Dashboard

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom'
 // @material-ui/core
 import withStyles from "@material-ui/core/styles/withStyles";
 // core components
@@ -12,14 +13,16 @@ import Card from "components/Card/Card.jsx";
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
 
 import InfiniteCalendar from 'react-infinite-calendar';
-import  {withMultipleDates, defaultMultipleDateInterpolation, Calendar } from 'react-infinite-calendar';
+import  {withMultipleDates, defaultMultipleDateInterpolation, Calendar} from 'react-infinite-calendar';
 import 'react-infinite-calendar/styles.css';
+
+const today = new Date();
 
 class Dashboard extends React.Component {
     state = {
         selectDate: [
             {
-                data: new Date(),
+                data: today
             },
             {
                 data: '2018, 10, 27'
@@ -37,10 +40,10 @@ class Dashboard extends React.Component {
       <div>
         <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-                <Button color="primary" fullWidth>ADD NEW EXERCISE</Button>
+                <Link to='/new_exercise'><Button color="primary" fullWidth>ADD NEW EXERCISE</Button></Link>
             </GridItem>
             <GridItem xs={12} sm={12} md={6}>
-                <Button color="primary" fullWidth>ADD NEW WORKOUT</Button>
+                <Link to='/new_workout'><Button color="primary" fullWidth>ADD NEW WORKOUT</Button></Link>
             </GridItem>
             <GridItem xs={12} sm={6} md={12}>
                 <Card>
@@ -51,6 +54,13 @@ class Dashboard extends React.Component {
                                 return item.data
                             })
                         }
+                        theme={{
+                            selectionColor: date => {
+                                console.log(date);
+                                console.log(today);
+                                return today ? '#EC6150' : '#26f026';
+                            },
+                        }}
                         width="100%"
                         interpolateSelection={defaultMultipleDateInterpolation}
                     />
